524fc64e83c220403390e43089b33c26d8071620
SumitGarg <garg10sumit@gmail.com>
2025-06-24 22:57:37 +0530

    feat: add /, /login, and /profile routes in app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
-  return <div>APP</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
