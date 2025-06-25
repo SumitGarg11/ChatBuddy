18764c4d4459985578e11437b930bf36da230b0b
SumitGarg <garg10sumit@gmail.com>
2025-06-26 00:38:21 +0530

    feat: add MongoDB connection utility using Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
-import express from "express"
-import "dotenv/config"
-import cors from "cors"
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
 import http from "http";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const server = http.createServer(app);
 
-
 // Middleware setup
 
-app.use(express.json({limit: "4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
-app.use("/api/status",(req,res)=> res.send("server is live"));
+ app.use("/api/status", (req, res) => res.send("server is live"));
+
+// connect to mongodb
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT,function(){
-    console.log("server is running on PORT: ", PORT)
+server.listen(PORT, function () {
+  console.log("server is running on PORT: ", PORT);
 });
