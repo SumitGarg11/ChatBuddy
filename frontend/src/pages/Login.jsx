495da4c6151f0475ac7a17ed7380061dbc2662db
SumitGarg <garg10sumit@gmail.com>
2025-06-25 19:05:25 +0530

    feat: create initial Login component layout with logo and form structure

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import assets from "../assets/assets";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [currState, setCurrState] = useState("Sign Up");
+
+  return (
+    <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl ">
+      {/* <--------- left ---------> */}
+      <img src={assets.chatlogo} alt="" className="w-[min(30vw,250px)]" />
+
+      {/* ------------- Right ----------- */}
+
+      <form
+        className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg  "
+        action=""
+      >
+        <h2></h2>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
