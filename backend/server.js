6d48228e6fbf0cd32188ca02c5ee6699c235ed95
SumitGarg <garg10sumit@gmail.com>
2025-06-28 01:33:45 +0530

    Integrate userRouter with /api/auth route in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 // --------- create express app and http server -----
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 app.use("/api/status", (req, res) => res.send("server is live"));
+app.use("api/auth",userRouter);
 
 // Connect to MongoDB
 await connectDB();
