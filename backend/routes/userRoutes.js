9369215b3a65c8b6715ac8585fddea6dcc773883
SumitGarg <garg10sumit@gmail.com>
2025-06-27 22:43:01 +0530

    chore: delete backend folder due to project restructure

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login, signup, updateProfile } from "../Controllers/userController";
-
-const userRouter = express.Router();
-userRouter.post("/signup", signup);
-userRouter.post("/login", login);
-userRouter.put("/update-profile/:userId", updateProfile);
-
-export default userRouter;
