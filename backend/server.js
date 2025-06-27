b98046e3822227b7a5d026eeffe4fcd9d8c33741
SumitGarg <garg10sumit@gmail.com>
2025-06-27 23:33:29 +0530

    Add db.js and configure MongoDB connection using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
-import express from "express"
+import express from "express";
 import "dotenv/config";
-import cors from "cors"
+import cors from "cors";
 import http from "http";
+import { connectDB } from "./lib/db.js";
 
-// --------- create express app and http server ----- 
+// --------- create express app and http server -----
 const app = express();
 const server = http.createServer(app);
 
-// ----- Middleware setup ----  
-app.use(express.json({limit: "4mb"}));
+// ----- Middleware setup ----
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
-app.use("/api/status",(req,res) => res.send("server is live"));
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, function(){
-    console.log("Server is running on Port on ", PORT);
-})
+app.use("/api/status", (req, res) => res.send("server is live"));
 
+// Connect to MongoDB
+await connectDB();
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, function () {
+  console.log("Server is running on Port on ", PORT);
+});
