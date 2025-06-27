f111cc5ab9779c1b94fa16b6edbce4ea22518b05
SumitGarg <garg10sumit@gmail.com>
2025-06-28 00:34:26 +0530

    Add user login controller with password verification and JWT token generation

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -25,3 +25,23 @@ export const signup = async(req,res) => {
 
     }
 }
+
+
+
+export const login = async (req,res) => {
+    try {
+        const { email, password} = req.body;
+        const userData = await User.findOne({email})
+
+        const isPasswordCorrect = await bcrypt.compare(password,userData.password);
+        if(!isPasswordCorrect){
+            return res.json({success: false, message: "Invalid credentials"});
+        }
+        const token = generateToken(userData._id);
+
+        res.json({suceess: true, userData, token, message: "Login Successful"})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+} 
