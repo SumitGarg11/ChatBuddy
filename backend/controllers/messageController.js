d022ff76baa0d75935a7cf4b7d843318d2e28555
SumitGarg <garg10sumit@gmail.com>
2025-06-28 12:15:12 +0530

    Add sendMessage controller to send text or image messages with Cloudinary upload support

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -3,6 +3,7 @@
 
 // Get All users except the logged in user 
 
+import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/Message.js";
 import User from "../models/User.js";
 
@@ -60,4 +61,30 @@ export const markMessageAsSeen = async (req, res) => {
         console.log(error.message);
         res.json({success: false, message: error.message})
     }
+}
+
+// send message to selected user
+
+export const sendMessage = async (req, res) => {
+    try {
+        const {text, image} = req.body;
+        const receiverId =  req.params.id;
+        const senderId =  req.user._id;
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadResponse.secure_url;
+        }
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+        res.json({success: true, newMessage });
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+
 }
